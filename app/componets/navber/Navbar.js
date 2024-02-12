303fcee48a1fd6c2893517dd8b0c3941f4d20535
Sakib <sahadothosain6619@gmail.com>
2024-02-12 16:15:16 +0600

    demo project

---
+++ app/componets/navber/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+const Navbar = () => {
+  return (
+    <>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+        </div>
+      </nav>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
